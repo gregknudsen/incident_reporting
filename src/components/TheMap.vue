47c87ad725ddf1d9c4e3c1e5f9bf12470dec740f
gregknudsen <dev.artist15@gmail.com>
2024-09-16 10:57:09 -0400

    add marker to coords

--- src/components/TheMap.vue
+++ src/components/TheMap.vue
@@ -6,14 +6,27 @@ const lat = ref(0);
 const lng = ref(0);
 const map = ref();
 const mapContainer = ref();
+// const marker = ref();
 
 onMounted(() => {
-  map.value = L.map(mapContainer.value).setView([51.505, -0.09], 13);
+  map.value = L.map(mapContainer.value).setView([37.466513, -77.428683], 13);
+  L.marker([37.466513, -77.428683]).addTo(map.value)
+    .on("mouseover", (e) => {
+      // e.preventDefault();
+      
+      console.log("HOVER", e);
+      
+    })
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map.value);
+
+  // marker.value = L.marker([lat.value, lng.value]).addTo(map.value)
+      // .on("dragend",(event)=> {
+      //   console.log(event)
+      // });
 });
 
 
