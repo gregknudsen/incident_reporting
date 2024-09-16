1a28b5710a06b468b2a8d2bb7160f25e96950f04
gregknudsen <dev.artist15@gmail.com>
2024-09-16 10:18:56 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import TheMap from './components/TheMap.vue'
+</script>
+
+<template>
+
+  <main>
+    <!-- <TheWelcome /> -->
+    <TheMap />
+  </main>
+</template>
+
+
---
+++ src/components/TheMap.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    THE MAP
+  </div>
+</template>
+
+<script setup>
+import data from '../data/F01705150050.json'
+
+console.log("DATA", data);
+
+
+</script>
